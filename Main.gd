05c698477cf6416a2700c3d096470310c0ba4733
annas2727 <annasicoli@aol.com>
2024-04-06 23:13:01 -0400

    Merged with Kelly's

--- Main.gd
+++ Main.gd
@@ -1,4 +1,21 @@
-extends Node
+extends Node2D
 
-# Main (set as main scene)
+var hud_enabled = true
+const maze = preload("res://tile_map.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+#func on_fringe_changed():
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("reset"):
+		var new_maze = maze.instantiate()
+		var existing_maze = get_children().filter(func(x):
+			return "TileMap" in x.name)[0]
+		existing_maze.queue_free()
+		add_child(new_maze)
+	
 
--- maze_gen.gd
+++ maze_gen.gd
@@ -12,8 +12,8 @@ var spot_to_label = {}
 var current_letter_num = 65
 #const label = preload("res://simple_label.tscn")
 
-@export var y_dim = 15
-@export var x_dim = 25
+@export var y_dim = 35
+@export var x_dim = 35
 @export var starting_coords = Vector2i(0, 0)
 var adj4 = [
 	Vector2i(-1, 0),
@@ -29,7 +29,6 @@ func _ready() -> void:
 	Globals.letters_to_show.clear()
 	place_border()
 	dfs(starting_coords)
-	
 
 func _input(event: InputEvent) -> void:
 	pass
@@ -120,4 +119,3 @@ func dfs(start: Vector2i):
 		#if we hit a dead end or are at a cross section
 		if not found_new_path:
 			place_wall(current)
-
